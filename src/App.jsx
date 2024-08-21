a9c57a72ed2a61ffc64506121ba3b1c5083029a6
manpreet <manpreet26simmak@gmail.com>
2024-08-21 19:31:27 +0530

    Minimalist portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ function App() {
       smooth: true, // Enable smooth scroll
       mouseMultiplier: 2,
       // Reduced for better mobile performance
-      smoothTouch: false, // Disable smooth scroll on touch devices
+      smoothTouch: true, // Disable smooth scroll on touch devices
       touchMultiplier: 2, // Multiplier for touch scrolling
       // Mouse scroll speed multiplier
     });
@@ -66,7 +66,7 @@ function App() {
         <section className="h-[auto]">
           <Projects />
         </section>
-        <section className="h-[100vh]">
+        <section className="h-[auto]">
           <Footer />
         </section>
       </div>
